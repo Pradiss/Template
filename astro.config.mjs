6401d4403f8f0b2adb65ed9b5a92a83bbd91e843
Erick <erick@Ericks-MacBook-Air.local>
2026-04-30 15:16:11 -0300

    first Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+export default defineConfig({
+  
+
+  integrations: [
+    sitemap(),
+    icon({
+      include: {
+        mdi: ['whatsapp', 'instagram', 'facebook'],
+      },
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
